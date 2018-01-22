e4498a77f86d6276aa8066d93b406c3a5bd9876e
Ted Morin <theodore.morin@clearwaterclinical.com>
2018-01-21 19:34:15 -0500

    Add asset prefix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
   exportPathMap: function() {
     return {
       '/': { page: '/' }
     }
-  }
+  },
+  assetPrefix: isProd ? '/pgts-verify-tool' : ''
 }
